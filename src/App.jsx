bd4eba65e01ffcd08072a1af8b1a347e537bd0c6
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-06-22 18:16:48 -0400

    created useState and loading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react";
+
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+
+  const fetchJobs = async()=>{
+    setIsLoading(true)
+    const response = await fetch(url);
+    const newJobs = await response.json()
+    setJobs(newJobs);
+    setIsLoading(false);
+
+  }
+
+  useEffect(()=>{
+    fetchJobs();
+  }, [])
+
+  console.log(jobs);
+
+  if(isLoading){
+
+    return <section className="jobs-center">
+              <div className="loading">
+              </div>
+    </section>
+  }
   return <h2>Tabs Starter</h2>;
 };
 export default App;
