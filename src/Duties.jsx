2ede6f007e65c9523ec372e9611424ea519f9fdc
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-06-22 19:04:48 -0400

    updated the duties component and job info

---
+++ src/Duties.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+
+const Duties = ({duties }) => {
+
+  console.log(duties[1]);
+  return (
+    <div>Duties</div>
+  )
+}
+
+export default Duties
